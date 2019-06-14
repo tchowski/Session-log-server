c7956380aa6ae1aac78e6807e8349673e3d5ca0c
Mathieu Olechowski <m.olechowski@hotmail.fr>
2019-06-14 14:59:32 +0200

    creditial: false

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*', headers: :any, methods: %i[get post put delete options head],
-                  credentials: true
+                  credentials: false
   end
 end
