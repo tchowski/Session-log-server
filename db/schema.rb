36209b675c1750106d898caa53f550410d1ae435
Mathieu Olechowski <m.olechowski@hotmail.fr>
2019-06-14 14:12:53 +0200

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_28_151159) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "pseudo"
     t.string "password_digest"
