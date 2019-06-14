a1190866f5d665e396616ea705f8bdb076d35624
Mathieu Olechowski <m.olechowski@hotmail.fr>
2019-06-14 16:14:27 +0200

    session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'blooming-shore-70311.herokuapp.com'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'blooming-shore-70311.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+end
