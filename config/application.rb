b573506afafb12e569ba9cefe2e8ac12c276ba90
Mathieu Olechowski <m.olechowski@hotmail.fr>
2019-06-14 16:56:48 +0200

    request headers

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module IndieStudioSessionLog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://registerindie.tk/',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS DELETE}.join(",")
+    }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
