2dbafc53a39260cb28ec5ce7ba7447bbeb99d0e2
Mathieu Olechowski <m.olechowski@hotmail.fr>
2019-06-15 17:27:21 +0200

    try

--- config/application.rb
+++ config/application.rb
@@ -12,13 +12,9 @@ module IndieStudioSessionLog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'https://registerindie.tk'
-        resource '*', headers: :any, methods: %i[get post delete options],
-                      credentials: true
-      end
-    end
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins '*'
-#     resource '*', headers: :any, methods: %i[get post put delete head],
-#                   credentials: false
-#   end
-#   allow do
-#     origins 'https://blooming-shore-70311.herokuapp.com'
-#     resource '*', headers: :any, methods: %i[get post put delete options head],
-#                   credentials: true
-#   end
+  #   allow do
+  #     origins '*'
+  #     resource '*', headers: :any, methods: %i[get post put delete head],
+  #                   credentials: false
+  #   end
+  #   allow do
+  #     origins 'https://blooming-shore-70311.herokuapp.com'
+  #     resource '*', headers: :any, methods: %i[get post put delete options head],
+  #                   credentials: true
+  #   end
+  allow do
+    origins 'https://registerindie.tk'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
 end
+Rails.application.config.action_controller.forgery_protection_origin_check = false
