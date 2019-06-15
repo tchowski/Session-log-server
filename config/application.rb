a95945f8fbf290a0ec345623dbe3139513e98a4e
Mathieu Olechowski <m.olechowski@hotmail.fr>
2019-06-15 16:43:45 +0200

    credential true

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module IndieStudioSessionLog
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'https://registerindie.tk'
-        resource '*', headers: :any, methods: %i[get post delete]
+        resource '*', headers: :any, methods: %i[get post delete],
+                      credentials: true
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
